d3a314b2d24b54d916a146a52974b5474287463f
Jesse Paris <jparis@gmail.com>
2025-08-14 14:39:36 +0200

    ignore error

--- src/webparts/qlikEmbed/QlikEmbedWebPart.ts
+++ src/webparts/qlikEmbed/QlikEmbedWebPart.ts
@@ -18,6 +18,7 @@ export interface IQlikEmbedWebPartProps {
 
 export default class QlikEmbedWebPart extends BaseClientSideWebPart<IQlikEmbedWebPartProps> {
 	private _isDarkTheme: boolean = false;
+	// @ts-ignore
 	private _environmentMessage: string = "";
 	private _sectionTagValue: string = "";
 
