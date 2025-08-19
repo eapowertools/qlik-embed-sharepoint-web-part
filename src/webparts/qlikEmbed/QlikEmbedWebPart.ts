8913e2a3899e350f2bc8832e4cbd674270e06188
Eamonn Harrington <ktk@qlik.com>
2025-08-19 10:43:02 -0400

    test change, added a comment on line 18

--- src/webparts/qlikEmbed/QlikEmbedWebPart.ts
+++ src/webparts/qlikEmbed/QlikEmbedWebPart.ts
@@ -15,7 +15,7 @@ export interface IQlikEmbedWebPartProps {
 	appID: string;
 	objectID: string;
 }
-
+// some change to be deleted later
 export default class QlikEmbedWebPart extends BaseClientSideWebPart<IQlikEmbedWebPartProps> {
 	private _isDarkTheme: boolean = false;
 	// @ts-expect-error: This is used in onInit(), but TS doesn't pick up the usage.
